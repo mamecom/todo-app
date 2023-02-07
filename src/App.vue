b9df9c00165dbd70b1e6529c0983718920706a17
mamecom <mamesan.jp@gmail.com>
2023-02-08 08:37:58 +0900

    ボタン実装完了

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,36 @@
 <template>
-  <div>
     <main-header/>
-    <task-new></task-new>
-      <router-view/>
-  </div>
-  <ul>
+    <main-body/>
+    <router-view/>
+  <!-- <ul>
     <li v-for="todo in filteredTodos" :key="todo.id">
       <input type="checkbox" v-model="todo.done">
       <span :class="{ done: todo.done }">{{ todo.text }}</span>
       <button @click="removeTodo(todo)">削除</button>
     </li> 
-  </ul>
-  <button @click="hideCompleted = !hideCompleted">
+  </ul> -->
+  <!-- <button @click="hideCompleted = !hideCompleted">
     {{ hideCompleted ? 'Show all' : 'Hide completed' }}
-  </button>
+  </button> -->
 </template>
 
 <script>
-import MainHeader from './components/MainHeader.vue'
-import TaskNew from './components/TaskNew.vue'
+import MainHeader from './components/pages/MainHeader.vue'
+import MainBody from './components/pages/MainBody.vue'
 
 export default {
   components: {
     MainHeader,
-    TaskNew
-  },
-  data() {
-    return {
-      newTodo: '',
-      hideCompleted: false,
-      todos: []
-    }
-  },
-  computed: {
-    filteredTodos() {
-      return this.hideCompleted ? this.todos.filter((t) => !t.done) : this.todos
-    }
-  },
-  methods: {
-    addTodo() {
-      this.todos.push({id: this.todos.length +1, text: this.newTodo, done: false})
-      this.newTodo = ''
-    },
-    removeTodo(todo) {
-      this.todos = this.todos.filter((t) => t !== todo)
-    }
+    MainBody,
   }
 }
+
 </script>
 
 <style lang="scss">
 body {
   margin: 0;
+  padding: 0;
 }
 
 .done {
---
+++ src/components/pages/MainHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class= "mb-auto">
+        <nav class= "navbar navbar-expand navbar-dark bg-dark justify-content-between">
+            <span class= "navbar-brand mb-0 h1">{{ title }}</span>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "MainHeader",
+        data() {
+            return {
+            title: "Todo管理"
+            }
+        }
+    }
+</script>
+
+<style>
+    .mb-auto {
+        background-color: #FAAB78;
+        padding: 20px;
+    }
+</style>
